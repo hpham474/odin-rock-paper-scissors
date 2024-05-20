6670513922211234b0b691dba6d4b5e5ef946af1
hpham474 <hpham474@gmail.com>
2024-05-19 19:51:17 -0500

    Create function to play game. Move function to play round into game function

--- main.js
+++ main.js
@@ -87,6 +87,70 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice === "rock") {
+            if(computerChoice === "rock") {
+                console.log("Tie! Rock equals Rock.");
+            }
+            else if(computerChoice === "paper") {
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+            } 
+            else if(computerChoice === "scissors") {
+                console.log("You win! Rock beats Scissors");
+                humanScore++;
+            }
+            else {
+                console.log("Weird choices have been made")
+            }
+        }
+        else if(humanChoice === "paper") {
+            if(computerChoice === "rock") {
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            }
+            else if(computerChoice === "paper") {
+                console.log("Tie! Paper equals Paper");
+            } 
+            else if(computerChoice === "scissors") {
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+            }
+            else {
+                console.log("Weird choices have been made")
+            }
+        }
+        else if(humanChoice === "scissors") {
+            if(computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+            }
+            else if(computerChoice === "paper") {
+                console.log("You win! Scissors beats Paper");
+                humanScore++;
+            } 
+            else if(computerChoice === "scissors") {
+                console.log("Tie! Scissors equals Scissors");
+            }
+            else {
+                console.log("Weird choices have been made")
+            }
+        }
+        else {
+            console.log("Weird choices have been made")
+        }
+    }
 
+    let humanScore = 0;
+    let computerScore = 0;
+    const playCount = 5;
+
+    for (let i = 0; i < playCount; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+}
+
+playGame();
