a77f20b6d4afdf306669dcec1b0020931fae8c22
hpham474 <hpham474@gmail.com>
2024-05-19 19:22:25 -0500

    Create function to get computer's choice

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+function getComputerChoice() {
+    result = Math.floor((Math.random()) * 3) + 1;
+
+    switch (result) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            return "error";
+    }
+}
